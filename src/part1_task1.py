e67726eb5571cd0ea819f802c5b97e4aecbfbf81
Claudio <10453981@polimi.it>
2019-04-22 17:57:58 -0500

    small update

--- src/part1_task1.py
+++ src/part1_task1.py
@@ -48,11 +48,33 @@ server = tf.train.Server(clusterinfo, job_name=FLAGS.job_name, task_index=FLAGS.
 if FLAGS.job_name == "ps":
     server.join()
 elif FLAGS.job_name == "worker":
-    #put your code here
 
+# Download the dataset
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+# Define the variables used: 
+# x =               input
+# y_true =           expected output
+# prediction =      soft-max prediction 
+
+x = tf.placeholder(mnist.train.images, dtype=tf.float32)
+y_true = tf.placeholder(mnist.train.labels, dtype=tf.float32)
+W = tf.variable( (none, ), dtype=tf.float32)       # TODO: initialize using random values
+b = tf.variable( (none, ) , dtype=tf.float32)       # TODO: initialize using random values
 
 prediction  = tf.nn.softmax(tf.matmul(x, W) + b)
-loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(prediction), reduction_indices=1))
+loss = tf.reduce_mean(-tf.reduce_sum(y_true*tf.log(prediction), reduction_indices=1))
+
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+for i in range(100):
+    _, loss_value = sess.run((train, loss))
+    print(loss_value)
+
+print(sess.run(y_pred))
